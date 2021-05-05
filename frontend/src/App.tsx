bc98278dce630c2bff1581657aea4f2025783e17
mrraxa01 <mrraxa01@gmail.com>
2021-05-05 10:35:37 -0300

    Add Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Olá mundo!</h1>
+      <h1 className="text-primary">Olá mundo!</h1>
     </div>
   );
 }
